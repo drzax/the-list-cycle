bac0ea18e9873afde81452148642a6aeeea611a7
Simon Elvery <simon@elvery.net>
2019-03-18 22:30:06 +1000

    Remove awkward dependency on twitter-list-manager

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const tlm = require('twitter-list-manager');
+const correspondents = require("./lib/correspondents");
+const listAdd = require("./lib/list-add");
+const listRemove = require("./lib/list-remove");
+const listMembers = require("./lib/list-members");
+const list = process.env.TWITTER_LIST || "cycle";
 
-const list = process.env.TWITTER_LIST || 'cycle';
-
-var newMembers = tlm.correspondents(process.env.TWITTER_WINDOW || '1-month');
-var oldMembers = tlm.listMembers(list);
+var newMembers = correspondents(process.env.TWITTER_WINDOW || "1-month");
+var oldMembers = listMembers(list);
 
 Promise.all([newMembers, oldMembers]).then(([newMembers, oldMembers]) => {
-  var adding, removing, keeping;
-
-  adding = newMembers.filter(name => oldMembers.indexOf(name) === -1);
-  removing = oldMembers.filter(name => newMembers.indexOf(name) === -1);
-  keeping = oldMembers.filter(name => newMembers.indexOf(name) > -1);
+  const adding = newMembers.filter(name => oldMembers.indexOf(name) === -1);
+  const removing = oldMembers.filter(name => newMembers.indexOf(name) === -1);
+  // const keeping = oldMembers.filter(name => newMembers.indexOf(name) > -1);
 
   if (removing.length) {
-    tlm
-      .listRemove(list, removing)
-      .then(() => console.log(`Removed: ${removing.join(', ')}`), err);
+    listRemove(list, removing).then(
+      () => console.log(`Removed: ${removing.join(", ")}`),
+      err
+    );
   } else {
-    console.log('Nothing to remove.');
+    console.log("Nothing to remove.");
   }
 
   if (adding.length) {
-    tlm
-      .listAdd(list, adding)
-      .then(() => console.log(`Added: ${adding.join(', ')}`), err);
+    listAdd(list, adding).then(
+      () => console.log(`Added: ${adding.join(", ")}`),
+      err
+    );
   } else {
-    console.log('Nothing to add.');
+    console.log("Nothing to add.");
   }
 }, err);
 
